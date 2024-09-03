e15e5f5aacd3bb0a3f0b90ca7ed887ee9173edcc
Markus Kuuse <markuskuuse01@gmail.com>
2024-09-03 17:35:59 +0300

    refactored code to allow for mpd implentation

---
+++ playlist.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"time"
+)
+
+/* func verifyDir(dir string) string { // verify if dir variables have suffix
+	if strings.HasSuffix(dir,"/") {
+		return dir
+	}
+	return dir+"/"
+} */
+
+func createM3U(cfg Config, name string, files []string) error {
+	f, err := os.OpenFile(cfg.PlaylistDir+name+".m3u", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		full_file := fmt.Sprintf("%s%s.mp3\n",cfg.Youtube.DownloadDir, file)
+		_, err := f.Write([]byte(full_file))
+		if err != nil {
+			log.Printf("Failed to write song to file: %s", err.Error())
+		}
+	}
+	return nil
+}
+
+func getPlaylistName(persist bool) string {
+	playlistName := "Discover-Weekly"
+	if persist {
+		year, week := time.Now().ISOWeek()
+		playlistName = fmt.Sprintf("%s-%v-Week%v", playlistName, year, week)
+	}
+	return playlistName
+}
+
+func createPlaylist(cfg Config, songs, files []string, system string) error {
+
+	if system != "subsonic" && system != "mpd" {
+		return fmt.Errorf("unsupported music system: %s", system)
+	}
+	
+	playlistName := getPlaylistName(cfg.Persist)
+
+	if !cfg.Persist {
+		if err := handlePlaylistDeletion(cfg, system); err != nil {
+			return fmt.Errorf("failed to delete playlist: %w", err)
+		}
+	}
+
+	switch system {
+	case "subsonic":
+
+		scan(cfg.Subsonic)
+		if err := subsonicPlaylist(cfg.Subsonic, songs, playlistName); err != nil {
+			return fmt.Errorf("failed to create subsonic playlist: %w", err)
+		}
+		return nil
+	
+	case "mpd": 
+
+		if err := createM3U(cfg, playlistName, files); err != nil {
+			return fmt.Errorf("failed to create M3U playlist: %w", err)
+		}
+		return nil
+	}
+	return fmt.Errorf("something very strange happened")
+}
+
+func handlePlaylistDeletion(cfg Config, system string) error {
+		deleteSongs(cfg.Youtube)
+		
+		switch system {
+		case "subsonic":
+				playlists, err := getDiscoveryPlaylist(cfg.Subsonic)
+				if err != nil {
+					return err
+				}
+
+				if err := delPlaylists(playlists, cfg.Subsonic); err != nil {
+					return err
+			}
+			return nil
+			
+		case "mpd":
+			// Way to delete .m3u file
+		}
+	return nil
+}
