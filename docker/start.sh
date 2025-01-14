bfee2bfe49c3edef3eb24cf73e32dffed00c0454
Markus Kuuse <markuskuuse01@gmail.com>
2025-01-14 17:11:52 +0200

    update yt-dlp before running explo

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Cron schedule is set by compose or build files
-echo "$CRON_SCHEDULE cd /opt/explo && ./explo >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
+echo "$CRON_SCHEDULE apt add --upgrade yt-dlp && cd /opt/explo && ./explo >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
 
 crond -f -l 2
