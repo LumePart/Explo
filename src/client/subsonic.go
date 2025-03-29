a996a24aa4695c92714d51a9bcae759013787556
Markus Kuuse <markuskuuse01@gmail.com>
2025-03-29 13:14:22 +0200

    subsonic refactor

--- src/client/subsonic.go
+++ src/client/subsonic.go
@@ -1,16 +1,19 @@
-package main
+package client
 
 import (
 	"fmt"
-	"log"
-	"time"
 	"strings"
+	"time"
 
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
 	"net/url"
+
+	"explo/src/config"
 	"explo/src/debug"
+	"explo/src/models"
+	"explo/src/util"
 )
 
 type FailedResp struct {
@@ -55,152 +58,162 @@ type Playlist struct {
 	CoverArt  string    `json:"coverArt"`
 }
 
+type Subsonic struct {
+	Token string
+	Salt string
+	Cfg config.ClientConfig
+}
 
-func (cfg *Credentials) genToken() {
-	
+func NewSubsonic(cfg config.ClientConfig) *Subsonic {
+	return &Subsonic{Cfg: cfg}
+}
+
+func (c *Subsonic) AddHeader() error {
+	return nil
+}
+
+func (c *Subsonic) GetAuth() error { // Generate salt and token
 	var salt = make([]byte, 6)
 
 
 	_, err := rand.Read(salt)
 	if err != nil {
-		log.Fatalf("failed to read salt: %s", err.Error())
+		return fmt.Errorf("failed to read salt: %s", err.Error())
 	}
 
 	saltStr := base64.RawURLEncoding.EncodeToString(salt)
-	passSalt := fmt.Sprintf("%s%s", cfg.Password, saltStr)
+	passSalt := fmt.Sprintf("%s%s", c.Cfg.Creds.Password, saltStr)
 
 	token := fmt.Sprintf("%x", md5.Sum([]byte(passSalt)))
 
-	cfg.Token = url.PathEscape(token)
-	cfg.Salt = url.PathEscape(saltStr)
-
-}
-
-func searchTrack(cfg Config, track Track) (string, error) {
-
-	searchQuery := fmt.Sprintf("%s %s %s", track.Title, track.Artist, track.Album)
-    
-    reqParam := fmt.Sprintf("search3?query=%s&f=json", url.QueryEscape(searchQuery))
-	
-    body, err := subsonicRequest(reqParam, cfg)
-    if err != nil {
-        return "", err
-    }
-    
-    var resp SubResponse
-    if err := parseResp(body, &resp); err != nil {
-        return "", err
-    }
-    
-
-	switch len(resp.SubsonicResponse.SearchResult3.Song) {
-	case 0:
-		return "", fmt.Errorf("no results found for %s", searchQuery)
-	case 1:
-		return resp.SubsonicResponse.SearchResult3.Song[0].ID, nil
-	default:
-		for _, song := range resp.SubsonicResponse.SearchResult3.Song {
-			if song.Title == track.Title {
-				return song.ID, nil
-			}
-		}
-		return "", fmt.Errorf("multiple songs found for: %s, but titles do not match with the actual track", searchQuery)
-	}
-}
-
-func subsonicPlaylist(cfg Config, tracks []Track) (string, error) {
-
-	var trackIDs string
-
-	for _, track := range tracks { // Get track IDs from app and format them
-		if track.ID == "" {
-			songID, err := searchTrack(cfg, track)
-			if songID  == "" || err != nil  { // if ID is empty, skip song
-				debug.Debug(fmt.Sprintf("could not get %s", track.File))
-				continue
-			}
-			track.ID = songID
-		}
-		trackIDs += "&songId="+track.ID
-	}
-	
-	reqParam := fmt.Sprintf("createPlaylist?name=%s%s&f=json", cfg.PlaylistName, trackIDs)
-	
-	body, err := subsonicRequest(reqParam, cfg)
-
-	var resp SubResponse
-	if err := parseResp(body, &resp); err != nil {
-        return "", err
-    }
-	if err != nil {
-		return "", err
-	}
-	return resp.SubsonicResponse.Playlist.ID, nil
-}
-
-func subsonicScan(cfg Config) error {
-	reqParam := "startScan?f=json"
-	
-	if _, err := subsonicRequest(reqParam, cfg); err != nil {
-		return err
-	}
+	c.Token = url.PathEscape(token)
+	c.Salt = url.PathEscape(saltStr)
 	return nil
 }
 
-func getDiscoveryPlaylist(cfg Config) ([]string, error) {
-	reqParam := "getPlaylists?f=json"
-
-	body, err := subsonicRequest(reqParam, cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	var resp SubResponse
-	if err := parseResp(body, &resp); err != nil {
-        return nil, err
-    }
-
-	var playlists []string
-	for _, playlist := range resp.SubsonicResponse.Playlists.Playlist {
-		if strings.Contains(playlist.Name, "Discover-Weekly") {
-			playlists = append(playlists, playlist.ID)
-
-		}
-	}
-	return playlists, nil
-}
-
-func updSubsonicPlaylist(cfg Config, ID, comment string) error {
-	reqParam := fmt.Sprintf("updatePlaylist?playlistId=%s&comment=%s&f=json",ID, url.QueryEscape(comment))
-
-	if _, err := subsonicRequest(reqParam, cfg); err != nil {
-		return err
-	}
+func (c *Subsonic) GetLibrary() error {
 	return nil
 }
 
-func delSubsonicPlaylists(playlists []string, cfg Config) error {
+func (c *Subsonic) AddLibrary() error {
+	return nil
+}
 
-	for _, id := range playlists {
-		reqParam := fmt.Sprintf("deletePlaylist?id=%s&f=json", id)
-		if _, err := subsonicRequest(reqParam, cfg); err != nil {
+func (c *Subsonic) SearchSongs(tracks []*models.Track) error {
+	for _, track := range tracks {
+		searchQuery := fmt.Sprintf("%s %s %s", track.Title, track.Artist, track.Album)
+
+		reqParam := fmt.Sprintf("search3?query=%s&f=json", url.QueryEscape(searchQuery))
+
+		body, err := subsonicRequest(reqParam, *c)
+		if err != nil {
 			return err
 		}
+
+		var resp SubResponse
+    	if err := util.ParseResp(body, &resp); err != nil {
+        return err
+		}
+
+		switch len(resp.SubsonicResponse.SearchResult3.Song) {
+		case 0:
+			debug.Debug(fmt.Sprintf("no results found for %s", searchQuery))
+		case 1:
+			track.ID = resp.SubsonicResponse.SearchResult3.Song[0].ID
+		default:
+			for _, song := range resp.SubsonicResponse.SearchResult3.Song {
+				if song.Title == track.Title || song.Title == track.CleanTitle {
+					track.ID = song.ID
+				}
+			}
+			debug.Debug(fmt.Sprintf("multiple songs found for: %s, but titles do not match with the actual track", searchQuery))
+		}
 	}
 	return nil
 }
 
-func subsonicRequest(reqParams string, cfg Config) ([]byte, error) {
+func (c *Subsonic) RefreshLibrary() error {
+	reqParam := "startScan?f=json"
+	
+	if _, err := subsonicRequest(reqParam, *c); err != nil {
+		return err
+	}
+	return nil
+}
 
-	reqURL := fmt.Sprintf("%s/rest/%s&u=%s&t=%s&s=%s&v=%s&c=%s", cfg.URL, reqParams, cfg.Creds.User, cfg.Creds.Token, cfg.Creds.Salt, cfg.Subsonic.Version, cfg.Subsonic.ID)
+func (c *Subsonic) CreatePlaylist(tracks []*models.Track) error {
+	var trackIDs strings.Builder
+	for _, track := range tracks { // build songID parameters
+		fmt.Fprintf(&trackIDs, "&songId=%s", track.ID)
+	}
 
-	body, err := makeRequest("GET", reqURL, nil, nil)
+	reqParam := fmt.Sprintf("createPlaylist?name=%s%s&f=json", c.Cfg.PlaylistName, trackIDs.String())
+
+	body, err := subsonicRequest(reqParam, *c)
+	if err != nil {
+		return err
+	}
+
+	var resp SubResponse
+	if err := util.ParseResp(body, &resp); err != nil {
+        return err
+    }
+	
+	c.Cfg.PlaylistID = resp.SubsonicResponse.Playlist.ID
+	return nil
+}
+
+func (c *Subsonic) SearchPlaylist() error {
+	reqParam := "getPlaylists?f=json"
+
+	body, err := subsonicRequest(reqParam, *c)
+	if err != nil {
+		return err
+	}
+
+	var resp SubResponse
+	if err := util.ParseResp(body, &resp); err != nil {
+        return err
+    }
+
+	for _, playlist := range resp.SubsonicResponse.Playlists.Playlist {
+		if playlist.Name == c.Cfg.PlaylistName {
+			c.Cfg.PlaylistID = playlist.ID
+			return nil
+
+		}
+	}
+	return nil
+}
+
+func (c *Subsonic) UpdatePlaylist(comment string) error {
+	reqParam := fmt.Sprintf("updatePlaylist?playlistId=%s&comment=%s&f=json",c.Cfg.PlaylistID, url.QueryEscape(comment))
+
+	if _, err := subsonicRequest(reqParam, *c); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Subsonic) DeletePlaylist() error {
+	reqParam := fmt.Sprintf("deletePlaylist?id=%s&f=json", c.Cfg.PlaylistID)
+
+	if _, err := subsonicRequest(reqParam, *c); err != nil {
+		return err
+	}
+	return nil
+}
+
+func subsonicRequest(reqParams string, c Subsonic) ([]byte, error) {
+
+	reqURL := fmt.Sprintf("%s/rest/%s&u=%s&t=%s&s=%s&v=%s&c=%s",c.Cfg.URL, reqParams, c.Cfg.Creds.User, c.Token, c.Salt, c.Cfg.Subsonic.Version, c.Cfg.ClientID)
+	body, err := util.MakeRequest("GET", reqURL, nil, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to make request %s", err.Error())
 	}
 
 	var checkResp FailedResp
-	if err = parseResp(body, &checkResp); err != nil {
+	if err = util.ParseResp(body, &checkResp); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal request %s", err.Error())
 	} else if checkResp.SubsonicResponse.Status == "failed" {
 		return nil, fmt.Errorf("%s", checkResp.SubsonicResponse.Error.Message)
