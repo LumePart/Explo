c15819fb0d4e280be506e1daf86cc125e25b3c7b
Markus Kuuse <markuskuuse01@gmail.com>
2025-04-21 19:04:49 +0300

    custom http client

--- src/util/http.go
+++ src/util/http.go
@@ -5,10 +5,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"time"
+
 	"explo/src/debug"
 )
 
-func MakeRequest(method, url string, payload io.Reader, headers map[string]string) ([]byte, error) {
+type HttpClientConfig struct {
+	Timeout        time.Duration
+}
+
+type HttpClient struct {
+	Client         *http.Client
+}
+
+func NewHttp(cfg HttpClientConfig) *HttpClient {
+	return &HttpClient{
+		Client: &http.Client{
+			Timeout: cfg.Timeout * time.Second,
+		},
+	}
+}
+
+func (c *HttpClient) MakeRequest(method, url string, payload io.Reader, headers map[string]string) ([]byte, error) {
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize request: %s", err.Error())
@@ -20,7 +38,7 @@ func MakeRequest(method, url string, payload io.Reader, headers map[string]strin
 		req.Header.Add(key,value)
 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := c.Client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to make request: %s", err.Error())
 	}
