5e5762b911263791e2c6d74e5cbd09bee6143bf8
LumePart <markuskuuse01@gmail.com>
2026-05-11 20:47:14 +0300

    fix linter

--- src/web/backend/server.go
+++ src/web/backend/server.go
@@ -124,8 +124,10 @@ func (s *Server) Start() error {
 func (s *Server) startJobs() {
 
 	coversDir := filepath.Join(s.cfg.WebDataDir, "cache", "covers")
-	s.cronJobs.RegisterCoverCleanup(
-		"0 3 * * *", coversDir, s.cfg.CacheSizeMB)
+	if err := s.cronJobs.RegisterCoverCleanup(
+		"0 3 * * *", coversDir, s.cfg.CacheSizeMB); err != nil {
+			slog.Warn("failed to register cover cleanup job", "err", err.Error())
+		}
 
 
 	s.cronJobs.Start()
