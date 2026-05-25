9e24a1bdfb11b6de5be0e284f8fa6592e82f9fc9
dammitjeff <44111923+dammitjeff@users.noreply.github.com>
2026-05-24 18:46:35 -0700

    refactor: normalize title comparison in SearchSongs functions and improve logging

--- src/util/sanitize.go
+++ src/util/sanitize.go
@@ -1,11 +1,22 @@
 package util
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 var (
 	filenameRe = regexp.MustCompile(`[^\p{L}\d._,\-]+`)
 	alnumRe    = regexp.MustCompile(`[^\p{L}\d]+`)
+	featTailRe = regexp.MustCompile(`(?i)\s*[\(\[\{]\s*(feat\.?|featuring|ft\.?|with)\s[^\)\]\}]*[\)\]\}]\s*$`)
 )
+
+// NormalizeTitle strips trailing (feat. …) annotations, lowercases,
+// and reduces to alphanumeric-only for fuzzy title comparison.
+func NormalizeTitle(s string) string {
+	s = featTailRe.ReplaceAllString(s, "")
+	return AlnumOnly(strings.ToLower(s))
+}
 
 // FilenameSafe replaces characters unsafe for filenames with '_'
 func FilenameSafe(s string) string {
